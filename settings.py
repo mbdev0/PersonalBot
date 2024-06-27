18064133a7124752f4a0a425a553d8996510c896
nassorh <nassorh.dev@gmail.com>
2024-06-27 23:58:45 +0100

    feature : telegram webhook

--- settings.py
+++ settings.py
@@ -5,3 +5,5 @@ load_dotenv()
 
 HTTP_NODE_URL = os.getenv('HTTP_NODE_URL')
 WEBSOCKET_NODE_URL = os.getenv('WEBSOCKET_NODE_URL')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
--- webhook.py
+++ webhook.py
@@ -1,10 +1,24 @@
 
 from Models.Coin import Coin
+from settings import BOT_TOKEN, CHAT_ID
+import requests
 
+def send_telegram_webhook(coin: Coin):
+    coin_info = format_coint_info(coin)
+    send_telegram_message(BOT_TOKEN, CHAT_ID, coin_info)
 
-# USING TELEGRAM OR DISCORD DOCUMENTATION CREATE A WEBHOOK, SEND THE COIN INFO
+def format_coint_info(coin: Coin) -> str:
+    # Need to be updated to a better format
+    return "\n".join([f"{key}: {value if value is not None else 'N/A'}" for key, value in coin.items()])
 
-# WHAT'S QUICKER? TELEGRAM OR DISCORD? -> FIND OUT WHOEVER PICKS UP
-def send_webhook(coin: Coin):
-    # send webhook with coin info
-    pass
+def send_telegram_message(bot_token: str, chat_id: str, message: str):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    data = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    response = requests.post(url, data=data)
+    print("Sent the following to telegram successfully:\n"+message)
+
+test_coin = {'name': '1st Blink Sydney Sweeney', 'symbol': 'blinkboobs', 'ipfs_url': 'https://cf-ipfs.com/ipfs/Qme9GvUAU53DZinju8areGH9cA9KsA1a6YRTKCpbeMsUkt', 'telegram_url': None, 'twitter_url': None, 'website_url': None}
+send_telegram_webhook(coin)
