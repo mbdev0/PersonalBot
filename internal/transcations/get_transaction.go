762a50bba3ef7e6f7425913763a35b564945b489
nassorh <nassorh.dev@gmail.com>
2024-08-01 12:44:45 +0100

    refactor : change file directories

---
+++ internal/transcations/get_transaction.go
@@ -0,0 +1,23 @@
+func GetTransaction(signature string) {
+	// transaction := getTransaction(signature)
+	// instruction_data := getMintInstructionData(transaction)
+	// coin := parseInstructionData(instruction_data)
+	// ipfs := getIPFS(coin.Ipfs)
+	// combine the coin and ipfs struct into TranscationData struct and return
+}
+
+func getTransaction(signature string) {
+	// logic to fetch the transaction using the http client
+}
+
+func getMintInstructionData(transaction dictionary) {
+	// logic to fetch the mint instruction data using the http client
+}
+
+func parseInstructionData(instruction_data dictionary) {
+	// logic to parse the instruction data into a coin struct
+}
+\
+func getIPFS(ipfs_url string) {
+	// logic to parse the instruction data into a coin struct
+}
