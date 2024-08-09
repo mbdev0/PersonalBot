95e26def235991d55d865e0f7ad40e346baa8d03
mbdev0 <mustafababar7@gmail.com>
2024-08-10 00:09:29 +0100

    chore: monitor will run as soon as application is started + bool within StartMonitor is true

--- main.go
+++ main.go
@@ -3,25 +3,11 @@ package main
 
 import (
 	"fmt"
-	"pump_fun/internal/monitoring/logsubscribe"
-	"sync"
+	"pump_fun/internal/monitoring"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-	var wg sync.WaitGroup
-	userClickedStart := true
 
-	// run the log subscribe function in a goroutine and wait for it to finish
-	// the idea is that this function will run when a user clicks a button on the frontend
-	if userClickedStart {
-		wg.Add(1)
-		go func() {
-			logsubscribe.LogSubscribe()
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-
+	monitoring.StartMonitor()
 }
