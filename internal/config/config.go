857e6ef0b2b508ac695d4ba845614b55746ad5a4
mbdev0 <mustafababar7@gmail.com>
2024-08-13 22:00:36 +0100

    feature: added Config package - "global" variables can now be saved in config.json

---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"encoding/json"
+	"log/slog"
+	"os"
+	"pump_fun/internal/logger"
+)
+
+var config *Config
+
+func LoadConfig() error {
+	file, err := os.ReadFile("configuration/config.json")
+	if err != nil {
+		logger.Log(slog.LevelError, "Error reading config file", slog.String("error: ", err.Error()))
+		return err
+	}
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		logger.Log(slog.LevelError, "Error unmarshalling config file", slog.String("error: ", err.Error()))
+		return err
+	}
+	return nil
+}
+
+func GetConfig() *Config {
+	return config
+}
