f2126c85e672b7061be967df44471baf05e1373c
mbdev0 <mustafababar7@gmail.com>
2025-01-06 10:21:36 +0000

    chore: added comments to explain reasoning for idl map generator

--- internal/launch/load_pumpfun_idl.go
+++ internal/launch/load_pumpfun_idl.go
@@ -2,6 +2,7 @@ package launch
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
 	"pump_fun/internal/logger"
@@ -10,8 +11,7 @@ import (
 )
 
 type IdlMap struct {
-	Instruction string
-	AccountMap  map[string]int
+	AccountMap map[string]int
 }
 
 var (
@@ -23,7 +23,7 @@ func GetIdlMap() map[string]IdlMap {
 	once.Do(func() {
 		loadPumpfunIdl()
 	})
-
+	fmt.Println(idlMapInstance)
 	return idlMapInstance
 }
 
@@ -56,18 +56,27 @@ func loadIdlIntoModel() (*models.PumpFunIdl, error) {
 	return &idl, nil
 }
 
+/*
+Due to the fact that we will need to continusly access the idl to find where the mint, user, and bondingCurve accounts are located
+we will generate a map/dict to store the index of the accounts in the instruction. This will allow us to easily access the accounts in O(1) time
+instead of having to loop through the accounts each time we need to access them.
+
+The dict/map will look in the format of:
+{buy : {mint: 0, user: 1, bondingCurve: 2}, create: {mint: 0, user: 1, bondingCurve: 2}}} etc
+Which will correspond to the order of how the accounts are stored in the idl.
+*/
 func generateIdlMap(idl *models.PumpFunIdl) map[string]IdlMap {
 	idlMap := make(map[string]IdlMap)
 	for _, instruction := range idl.Instructions {
 		for num, account := range instruction.Accounts {
 			if _, ok := idlMap[instruction.Name]; !ok {
 				idlMap[instruction.Name] = IdlMap{
-					Instruction: instruction.Name,
-					AccountMap:  make(map[string]int),
+					AccountMap: make(map[string]int),
 				}
 			}
 			idlMap[instruction.Name].AccountMap[account.Name] = num
 		}
 	}
+
 	return idlMap
 }
