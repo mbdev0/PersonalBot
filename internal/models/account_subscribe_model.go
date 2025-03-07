b1bed7f6d7d4bc7ed2048f324669c7ff2a3e366d
mbdev0 <mustafababar7@gmail.com>
2025-03-07 04:35:08 +0000

    feature: added monitoring via geyser + rpc client of the account data changes

---
+++ internal/models/account_subscribe_model.go
@@ -0,0 +1,22 @@
+package models
+
+type AccountSubscribeModel struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Method  string `json:"method"`
+	Params  struct {
+		Subscription uint64 `json:"subscription"`
+		Result       struct {
+			Context struct {
+				Slot int `json:"slot"`
+			} `json:"context"`
+			Value struct {
+				Lamports   uint64   `json:"lamports"`
+				Data       []string `json:"data"`
+				Owner      string   `json:"owner"`
+				Executable bool     `json:"executable"`
+				RentEpoch  uint64   `json:"rentEpoch"`
+				Space      int      `json:"space"`
+			} `json:"value"`
+		} `json:"result"`
+	} `json:"params"`
+}
