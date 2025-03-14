0d4be14eee4e04239d8dfe2e958176d3356cdf77
nassorh <nassorh.dev@gmail.com>
2025-03-14 12:38:57 +0000

    refactor: moved from outer variable to parameter to avoid race conditions
    https://www.reddit.com/r/golang/comments/1gju1jj/is_there_any_difference_of_using_variables_from/?rdt=43831

--- internal/monitoring/afk_monitoring.go
+++ internal/monitoring/afk_monitoring.go
@@ -21,27 +21,27 @@ func StartAFKMonitor() {
 			transaction_notification_chan := make(chan models.TransactionNotification, 1000)
 			coinStructChan := make(chan models.Coin, 1000)
 
-			go func() {
+			go func(transaction_notification_chan chan models.TransactionNotification) {
 				err := geyser.Geyser_Stream_Transactions(transaction_notification_chan)
 				if err != nil {
 					logger.Error(err)
 				}
-			}()
+			}(transaction_notification_chan)
 
-			go func() {
+			go func(transaction_notification_chan chan models.TransactionNotification, coinStructChan chan models.Coin) {
 				for transaction := range transaction_notification_chan {
-					go func() {
+					go func(transaction models.TransactionNotification, coinStructChan chan models.Coin) {
 						handlers.HandleTransactionNotification(transaction, coinStructChan)
-					}()
+					}(transaction, coinStructChan)
 				}
 
-			}()
+			}(transaction_notification_chan, coinStructChan)
 
 			for coinStruct := range coinStructChan {
 				logger.Log(logger.LevelInfo, "Coin Struct: ", logger.String("Coin", coinStruct.CoinData.Name))
-				go func() {
+				go func(coin models.Coin) {
 					webhook.SendWebhook(&coinStruct)
-				}()
+				}(coinStruct)
 			}
 
 			wg.Done()
