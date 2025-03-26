a73607f1d182868b80c6c010a5b4bfbe6550e672
nassorh <nassorh.dev@gmail.com>
2025-03-26 15:42:34 +0000

    chore: removed function no longer used

--- internal/monitoring/transactions/transaction_decoder/buy_instruction_decoder.go
+++ internal/monitoring/transactions/transaction_decoder/buy_instruction_decoder.go
@@ -3,40 +3,7 @@ package transaction_decoder
 import (
 	"bytes"
 	"errors"
-	"pump_fun/internal/constants"
-	"pump_fun/internal/models"
 )
-
-// TODO: Remove the below if my understanding is correct
-func DecodeBuyInstruction(coin *models.Coin, data []byte) error {
-	if len(data) < 8 {
-		return nil
-	}
-
-	if bytes.Equal(data[:8], constants.BuyInstructionDiscriminator[:]) {
-		err := buy_decoder(data, coin)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// TODO: Remove the below if my understanding is correct
-func buy_decoder(data []byte, coinModel *models.Coin) error {
-	var err error
-	buf := bytes.NewBuffer(data[8:])
-
-	amount, err := convertToUint64(buf)
-	if err != nil {
-		return err
-	}
-
-	coinModel.CoinData.DevHoldingAmount = (float64(amount) / 1e17) * 100
-
-	return nil
-}
 
 func ExtractBuyAmountFromBuyInstruction(data []byte) (float64, error) {
 	if len(data) < 8 {
