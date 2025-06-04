2c4e2963a87fbeca75348efd2946bb0bc865070a
nassorh <nassorh.dev@gmail.com>
2025-06-04 11:10:23 +0100

    feature: added sell discriminator and sell task model

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -10,6 +10,7 @@ const (
 var (
 	CreateInstructionDiscriminator = [8]byte{24, 30, 200, 40, 5, 28, 7, 119}
 	BuyInstructionDiscriminator    = [8]byte{102, 6, 61, 18, 1, 218, 235, 234}
+	SellInstructionDiscriminator   = [8]byte{51, 230, 133, 164, 1, 127, 131, 173}
 )
 
 const (
---
+++ internal/models/tasks/sell_task.go
@@ -0,0 +1,11 @@
+package tasks
+
+import "github.com/gagliardetto/solana-go"
+
+type SellTask struct {
+	PublicKey    solana.PublicKey
+	TokenAddress solana.PublicKey
+	Wallet       solana.PrivateKey
+	Amount       int
+	MinSolAmount int
+}
