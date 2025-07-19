f206e30a5e857d9e79cc68c4841da2ac4c1bfec9
mbdev0 <mustafababar7@gmail.com>
2025-07-19 17:44:12 +0100

    feat: added confirmation if transaction was successful or not

---
+++ internal/rpc_client/confirm_transaction.go
@@ -0,0 +1,60 @@
+package rpcclient
+
+import (
+	"context"
+	"fmt"
+	"pump_fun/internal/logger"
+	"time"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+)
+
+const (
+	contextTimeout   = 30 * time.Second
+	pollInterval     = 2 * time.Second
+	maxConfirmations = 31
+)
+
+func ConfirmTransaction(sig solana.Signature) (IsSuccess bool, err error) {
+	client := GetClient()
+
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+		defer cancel()
+
+		txResp, err := client.GetSignatureStatuses(ctx, true, sig)
+		if err != nil {
+			return false, err
+		}
+
+		if len(txResp.Value) == 0 || txResp.Value[0] == nil {
+			logger.Information("Transaction status unavailable. Retrying...")
+			time.Sleep(pollInterval)
+			continue
+		}
+
+		status := txResp.Value[0]
+		if status.Err != nil {
+			return false, fmt.Errorf("transaction failed: %v", status.Err)
+		}
+
+		switch status.ConfirmationStatus {
+		case rpc.ConfirmationStatusFinalized:
+			return true, nil
+		case rpc.ConfirmationStatusConfirmed:
+			logger.Information(fmt.Sprintf("Transaction confirmed: %d/%d confirmations",
+				*status.Confirmations, maxConfirmations))
+
+			expired, err := IsBlockhashExpired(status.Slot)
+			if err != nil {
+				return false, fmt.Errorf("blockhash expiration check failed: %w", err)
+			}
+			if expired {
+				return false, fmt.Errorf("blockhash expired, re-send the tx")
+			}
+
+			time.Sleep(pollInterval)
+		}
+	}
+}
