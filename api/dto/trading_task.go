4f5ac92062c5a829f359c41b725195e12a761082
mbdev0 <mustafababar7@gmail.com>
2025-09-01 00:26:39 +0100

    bugfix: return task id + task type & for requests to the ep we can now specify the proper task we want

--- api/dto/trading_task.go
+++ api/dto/trading_task.go
@@ -7,30 +7,31 @@ const (
 )
 
 type TradingTask struct {
-	Type         TradingType
-	BuyAmount    float64 `json:"buy_amount"`
-	BuyFee       float64 `json:"buy_fee"`
-	ComputeUnits float64 `json:"compute_units"`
-	Slippage     float64 `json:"slippage"`
-	Wallet       string  `json:"wallet"`
-	Filters      Filters `json:"filters"`
+	Type         TradingType `json:"trading_type"`
+	BuyAmount    float64     `json:"buy_amount"`
+	BuyFee       float64     `json:"buy_fee"`
+	ComputeUnits float64     `json:"compute_units"`
+	Slippage     float64     `json:"slippage"`
+	Wallet       string      `json:"wallet"`
+	Filters      Filters     `json:"filters"`
 }
 
 type TradingTaskResponse struct {
-	Type         TradingType
-	BuyAmount    float64 `json:"buy_amount"`
-	BuyFee       float64 `json:"buy_fee"`
-	ComputeUnits float64 `json:"compute_units"`
-	Slippage     float64 `json:"slippage"`
-	Filters      Filters `json:"filters"`
+	Type         TradingType `json:"trading_type"`
+	Id           string      `json"id"`
+	BuyAmount    float64     `json:"buy_amount"`
+	BuyFee       float64     `json:"buy_fee"`
+	ComputeUnits float64     `json:"compute_units"`
+	Slippage     float64     `json:"slippage"`
+	Filters      Filters     `json:"filters"`
 }
 
 type TradingTaskPatch struct {
-	Type         *TradingType
-	BuyAmount    *float64 `json:"buy_amount"`
-	BuyFee       *float64 `json:"buy_fee"`
-	ComputeUnits *float64 `json:"compute_units"`
-	Slippage     *float64 `json:"slippage"`
-	Wallet       *string  `json:"wallet"`
-	Filters      *Filters `json:"filters"`
+	Type         *TradingType `json:"trading_type"`
+	BuyAmount    *float64     `json:"buy_amount"`
+	BuyFee       *float64     `json:"buy_fee"`
+	ComputeUnits *float64     `json:"compute_units"`
+	Slippage     *float64     `json:"slippage"`
+	Wallet       *string      `json:"wallet"`
+	Filters      *Filters     `json:"filters"`
 }
