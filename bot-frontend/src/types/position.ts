a2deb90980331daccbe09b8c9df76e1a740f438f
mbdev0 <mustafababar7@gmail.com>
2025-10-05 05:40:16 +0100

    task: init for frontend

---
+++ bot-frontend/src/types/position.ts
@@ -0,0 +1,9 @@
+interface PositionDto {
+    position_id: string
+    token_address: string
+    wallet_address: string
+    initial_token_amount: string
+    tokens_remaining: string
+    remaining_cost_basis: string
+    finalized_profit: string
+}
