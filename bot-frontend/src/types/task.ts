9ada81ebeb0ee995e6811fbdfaabca4a08f1e1bf
mbdev0 <mustafababar7@gmail.com>
2025-11-04 22:15:56 +0000

    added mvp of task creation

---
+++ bot-frontend/src/types/task.ts
@@ -0,0 +1,55 @@
+interface TaskDto {
+  task_id: string;
+  type: string;
+  slippage: number;
+  compute_units: number;
+  token_address: string;
+  buy_amount: number | undefined;
+  buy_fee: number | undefined;
+  sell_amount: number | undefined;
+  sell_fee: number | undefined;
+  sell_posiiton_id: string | undefined;
+  state: StateDto;
+}
+
+interface StateDto {
+  task_state: string;
+  error: string;
+}
+
+interface Task {
+  task_id: string;
+  type: string;
+  slippage: number;
+  compute_units: number;
+  token_address: string;
+  buy_amount: number | undefined;
+  buy_fee: number | undefined;
+  sell_amount: number | undefined;
+  sell_fee: number | undefined;
+  sell_posiiton_id: string | undefined;
+  state: State;
+}
+
+interface State {
+  task_state: TaskType;
+  error: string;
+}
+
+enum TaskType {
+  task_create = "TaskCreate",
+  task_run = "TaskRun",
+  tx_instruction_build = "TxInstructionBuild",
+  tx_build = "TxBuild",
+  tx_send = "TxSend",
+  tx_confirm = "TxConfirm",
+  task_done = "Done",
+  task_cancel = "TaskCancel",
+  task_validation_failed = "TaskValidationFailed",
+  tx_instruction_build_failed = "TxInstructionBuildFailed",
+  tx_build_failed = "TxBuildFailed",
+  tx_send_failed = "TxSendFailed",
+  tx_failed = "TxFailed",
+}
+
+export { type TaskDto, type Task, TaskType };
