72a891895fad302f81f345c7fe33062989cf06b4
mbdev0 <mustafababar7@gmail.com>
2025-12-15 07:27:13 +0000

    refactor + feat: added tanstack query + changed folder structure slightly

--- bot-frontend/src/App.tsx
+++ bot-frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
-import CreateTask from './components/taskEntry';
+import WalletDashboard from './features/wallets/components/walletDashboard';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Trading Bot</h1>
-      <CreateTask />
-    </>
+      {/* <CreateTask /> */}
+      <WalletDashboard />
+    </QueryClientProvider>
   );
 }
 
