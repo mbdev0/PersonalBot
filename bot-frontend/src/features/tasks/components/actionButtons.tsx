b0a16ed928533c17d510224619d35c0be04d6c63
mbdev0 <mustafababar7@gmail.com>
2026-01-23 17:07:26 +0000

    chore: changed action buttons to use shadcn style buttons

--- bot-frontend/src/features/tasks/components/actionButtons.tsx
+++ bot-frontend/src/features/tasks/components/actionButtons.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@/components/ui/button';
 import type { RowActions } from '../types/rowActions';
 import type { Row } from '../types/tableRows';
+import { ButtonGroup } from '@/components/ui/button-group';
 
 export interface ActionButtonProps {
   row: Row;
@@ -11,42 +13,52 @@ export interface ActionButtonProps {
 export function ActionButtons({ row, isRunning, setIsRunning, rowActions }: ActionButtonProps) {
   return (
     <>
-      {isRunning ? (
-        <button
-          className="stop_task"
-          onClick={() => {
-            setIsRunning(false);
-            rowActions.onStop(row);
-          }}
-        >
-          ⏹️
-        </button>
-      ) : (
-        <button
-          className="start_task"
-          onClick={() => {
-            setIsRunning(true);
-            rowActions.onStart(row);
-          }}
-        >
-          ▶️
-        </button>
-      )}
+      <ButtonGroup>
+        <ButtonGroup>
+          {isRunning ? (
+            <Button
+              className="stop_task"
+              onClick={() => {
+                setIsRunning(false);
+                rowActions.onStop(row);
+              }}
+            >
+              ⏹️
+            </Button>
+          ) : (
+            <Button
+              className="start_task"
+              onClick={() => {
+                setIsRunning(true);
+                rowActions.onStart(row);
+              }}
+            >
+              ▶️
+            </Button>
+          )}
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button
+            className="edit"
+            onClick={() => {
+              rowActions.onEdit(row);
+            }}
+          >
+            ✏️
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button className="delete" onClick={() => rowActions.onDelete(row)}>
+            🗑️
+          </Button>
+        </ButtonGroup>
 
-      <button
-        className="edit"
-        onClick={() => {
-          rowActions.onEdit(row);
-        }}
-      >
-        ✏️
-      </button>
-      <button className="delete" onClick={() => rowActions.onDelete(row)}>
-        🗑️
-      </button>
-      <button className="duplicate" onClick={() => rowActions.onDuplicate(row)}>
-        ⿻
-      </button>
+        <ButtonGroup>
+          <Button className="duplicate" onClick={() => rowActions.onDuplicate(row)}>
+            ⿻
+          </Button>
+        </ButtonGroup>
+      </ButtonGroup>
     </>
   );
 }
