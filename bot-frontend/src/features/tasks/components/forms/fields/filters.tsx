d84e26ee3ce05410f3003be73f9a430b966b9ca6
mbdev0 <mustafababar7@gmail.com>
2026-01-25 01:28:19 +0000

    refactor: made filters entry a bit more stupid

--- bot-frontend/src/features/tasks/components/forms/fields/filters.tsx
+++ bot-frontend/src/features/tasks/components/forms/fields/filters.tsx
@@ -1,4 +1,3 @@
-import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Switch } from '@/components/ui/switch';
 import type { Filters } from '@/features/tasks/types/strategyTask';
@@ -24,50 +23,44 @@ export function FiltersEntry({ filters, onFiltersChange }: FiltersEntryProps) {
       [key]: input,
     });
   };
-
   return (
-    <div>
-      <Label htmlFor="filters">Filters</Label>
-      <Card>
-        <div className="flex flex-row justify-evenly">
-          <div className="flex flex-col items-center">
-            <Label htmlFor="hasWebsite">Has Website</Label>
-            <Switch
-              id="has-website"
-              checked={filters.has_website ?? false}
-              onCheckedChange={(checked) => updateFilter('has_website', checked)}
-            ></Switch>
-          </div>
+    <div className="flex flex-row justify-evenly flex-wrap">
+      <div className="flex flex-col items-center">
+        <Label htmlFor="hasWebsite">Has Website</Label>
+        <Switch
+          id="has-website"
+          checked={filters.has_website ?? false}
+          onCheckedChange={(checked) => updateFilter('has_website', checked)}
+        ></Switch>
+      </div>
 
-          <div className="flex flex-col items-center">
-            <Label htmlFor="hasTelegram">Has Telegram</Label>
-            <Switch
-              id="has-telegram"
-              checked={filters.has_telegram ?? false}
-              onCheckedChange={(checked) => updateFilter('has_telegram', checked)}
-            ></Switch>
-          </div>
+      <div className="flex flex-col items-center">
+        <Label htmlFor="hasTelegram">Has Telegram</Label>
+        <Switch
+          id="has-telegram"
+          checked={filters.has_telegram ?? false}
+          onCheckedChange={(checked) => updateFilter('has_telegram', checked)}
+        ></Switch>
+      </div>
 
-          <div className="flex flex-col items-center">
-            <Label htmlFor="hasTwitter">Has Twitter</Label>
-            <Switch
-              id="has-twitter"
-              checked={filters.has_twitter ?? false}
-              onCheckedChange={(checked) => updateFilter('has_twitter', checked)}
-            ></Switch>
-          </div>
+      <div className="flex flex-col items-center">
+        <Label htmlFor="hasTwitter">Has Twitter</Label>
+        <Switch
+          id="has-twitter"
+          checked={filters.has_twitter ?? false}
+          onCheckedChange={(checked) => updateFilter('has_twitter', checked)}
+        ></Switch>
+      </div>
 
-          <div className="flex flex-col items-center">
-            <Label htmlFor="devWallet">Dev Wallet</Label>
-            <Input
-              id="devWallet"
-              value={filters.dev_wallet ?? ''}
-              placeholder="Developer Wallet"
-              onChange={(e) => updateInputFilter('dev_wallet', e.target.value)}
-            ></Input>
-          </div>
-        </div>
-      </Card>
+      <div className="flex flex-col items-center">
+        <Label htmlFor="devWallet">Dev Wallet</Label>
+        <Input
+          id="devWallet"
+          value={filters.dev_wallet ?? ''}
+          placeholder="Developer Wallet"
+          onChange={(e) => updateInputFilter('dev_wallet', e.target.value)}
+        ></Input>
+      </div>
     </div>
   );
 }
