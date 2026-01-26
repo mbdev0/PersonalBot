2931fee06048c760f94ce0baeabdb4b15c500a51
mbdev0 <mustafababar7@gmail.com>
2026-01-26 22:44:28 +0000

    feat: added dark mode - eyes were hating light mode

--- bot-frontend/src/App.tsx
+++ bot-frontend/src/App.tsx
@@ -1,26 +1,29 @@
 import './App.css';
+import { ThemeProvider } from './components/themeProvider';
 import { TaskDashboard } from './features/tasks/components/taskDashboard';
-import WalletDashboard from './features/wallets/components/walletDashboard';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen bg-background">
-        <div className="max-w-7xl mx-auto py-8">
-          <header>
-            <h1 className="text-3xl font-bold">Trading Bot</h1>
-            <p>Tasks</p>
-          </header>
-          <div className="rounded-xl shadow-lg border bg-secondary p-5">
-            <TaskDashboard />
-            {/* <WalletDashboard /> */}
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <div className="min-h-screen bg-background">
+          <div className="max-w-7xl mx-auto py-8">
+            <header>
+              <h1 className="text-3xl font-bold">Trading Bot</h1>
+              <p>Tasks</p>
+            </header>
+            <div className="rounded-xl shadow-lg border bg-secondary p-5">
+              <TaskDashboard />
+              {/* <WalletDashboard /> */}
+            </div>
           </div>
         </div>
-      </div>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
