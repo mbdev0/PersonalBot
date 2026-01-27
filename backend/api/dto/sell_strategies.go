f6d5d0613a0c5ba4d860bc40189fdf6eccd5445c
mbdev0 <mustafababar7@gmail.com>
2026-01-27 23:52:38 +0000

    task: Added id to strategies for FE editing etc.

--- backend/api/dto/sell_strategies.go
+++ backend/api/dto/sell_strategies.go
@@ -1,6 +1,7 @@
 package dto
 
 type SellStrategyDTO struct {
+	Id         string  `json:"id"`
 	Type       string  `json:"type"`
 	Value      float64 `json:"value"`
 	SellAmount float64 `json:"sell_amount"`
