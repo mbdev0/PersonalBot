9e7a9dd92c697518ff9038d3758d46348202b7ad
mbdev0 <mustafababar7@gmail.com>
2026-01-31 03:04:13 +0000

    feat: updated dashboard e.p. - need to update front end to ingest this

--- backend/api/dto/dashboard.go
+++ backend/api/dto/dashboard.go
@@ -5,3 +5,55 @@ type DashboardResponse struct {
 	TasksByStrategy map[int64][]ResponseTask `json:"tasksByStrategy"`
 	ManualTasks     []ResponseTask           `json:"manualTasks"`
 }
+
+type TableRow struct {
+	Type      string
+	Id        int64
+	WsMessage string
+	State     string
+	Data      TradingTaskResponse //the trading task itself holding all options
+	Children  []ChildRow          //a table row can/cannot have children depending on type
+}
+
+type ChildRow struct {
+	Type      string
+	Id        int
+	WsMessage string
+	State     string
+	Data      ResponseTask
+}
+
+type DashboardResponseDto struct {
+	Rows []TableRow
+}
+
+//dashboard response should be
+
+/*
+{strategies: [
+	{type: afk,
+	id: 123,
+	msg: null //->fill this in with w.s. messages
+	state: string
+	data: StrategyTask
+	children:[
+		{type: buyTask, id: 123, data: BuyTask} -> this needs to be the same format as the actual strategy -> ???}
+		]
+	},
+	{type: buy,
+	id: 123,
+	data: BuyTask,
+	msg: null //->fill this in with w.s. messages
+	state: use data.state?
+
+	},
+	{type:sell,
+	id: 123,
+	data:SellTask,
+	msg: null //->fill this in with w.s. messages
+	state: use data.state?
+	}
+}
+]}
+
+*/
