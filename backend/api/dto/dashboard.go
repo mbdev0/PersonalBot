795ee944716d76c21455a7f3560536568cccecda
mbdev0 <mustafababar7@gmail.com>
2026-02-01 05:17:23 +0000

    refactor: small clean up of dashboard

--- backend/api/dto/dashboard.go
+++ backend/api/dto/dashboard.go
@@ -11,8 +11,8 @@ type TableRow struct {
 	Id        int64               `json:"id"`
 	WsMessage string              `json:"wsMessage"`
 	State     string              `json:"state"`
-	Data      TradingTaskResponse `json:"data"`     //the trading task itself holding all options
-	Children  []ChildRow          `json:"children"` //a table row can/cannot have children depending on type
+	Data      TradingTaskResponse `json:"data"`
+	Children  []ChildRow          `json:"children"`
 }
 
 type ChildRow struct {
@@ -27,33 +27,9 @@ type DashboardResponseDto struct {
 	Rows []TableRow `json:"rows"`
 }
 
-//dashboard response should be
+type RowType string
 
-/*
-{strategies: [
-	{type: afk,
-	id: 123,
-	msg: null //->fill this in with w.s. messages
-	state: string
-	data: StrategyTask
-	children:[
-		{type: buyTask, id: 123, data: BuyTask} -> this needs to be the same format as the actual strategy -> ???}
-		]
-	},
-	{type: buy,
-	id: 123,
-	data: BuyTask,
-	msg: null //->fill this in with w.s. messages
-	state: use data.state?
-
-	},
-	{type:sell,
-	id: 123,
-	data:SellTask,
-	msg: null //->fill this in with w.s. messages
-	state: use data.state?
-	}
-}
-]}
-
-*/
+const (
+	STRATEGY RowType = "strategy"
+	TASK     RowType = "task"
+)
