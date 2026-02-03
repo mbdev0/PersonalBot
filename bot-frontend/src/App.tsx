0be28491c4d88570bb7aed207ea4d0bebd3e397c
mbdev0 <mustafababar7@gmail.com>
2026-02-03 23:46:28 +0000

    refactor: moved sidebar to it's own component

--- bot-frontend/src/App.tsx
+++ bot-frontend/src/App.tsx
@@ -1,21 +1,10 @@
-import { Coins, Wallet, Bot } from 'lucide-react';
 import { ThemeProvider } from './components/themeProvider';
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarProvider,
-} from './components/ui/sidebar';
+import { SidebarProvider } from './components/ui/sidebar';
 import { TaskDashboard } from './features/tasks/components/taskDashboard';
-import { BrowserRouter, Link, Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import WalletDashboard from './features/wallets/components/walletDashboard';
+import { AppSidebar } from './components/appSidebar';
 
 const queryClient = new QueryClient();
 
@@ -27,9 +16,9 @@ function App() {
           <SidebarProvider defaultOpen={false}>
             <AppSidebar />
 
-            <div className="min-h-screen bg-background">
-              <div className="max-w-7xl mx-auto py-8">
-                <header>
+            <div className="min-h-screen bg-background w-full flex justify-center">
+              <div className="max-w-11/12 w-full py-8">
+                <header className="mb-8">
                   <h1 className="text-3xl font-bold">Trading Bot</h1>
                 </header>
                 <Routes>
@@ -46,73 +35,3 @@ function App() {
 }
 
 export default App;
-
-export function AppSidebar() {
-  const navMain = [
-    {
-      title: 'Task Dashboard',
-      url: '/',
-      icon: Coins,
-      isActive: true,
-    },
-    {
-      title: 'Wallets',
-      url: '/wallets',
-      icon: Wallet,
-      isActive: false,
-    },
-  ];
-
-  return (
-    <Sidebar
-      collapsible="none"
-      className="w-[calc(var(--sidebar-width-icon)+1px)] border-r min-h-screen"
-    >
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              tooltip={{ children: 'Bot - by Mustafa' }}
-              size="lg"
-              asChild
-              className="md:h-8 md:p-0"
-            >
-              <a href="#">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Bot className="size-4" />
-                </div>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent className="px-1.5 md:px-0">
-            <SidebarMenu>
-              {navMain.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton
-                    tooltip={{
-                      children: item.title,
-                      hidden: false,
-                    }}
-                    isActive={location.pathname === item.url}
-                    asChild
-                  >
-                    <Link to={item.url}>
-                      {' '}
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter />
-    </Sidebar>
-  );
-}
