9ed5de9bd10edae3940a81103c792f336242b7b6
mbdev0 <mustafababar7@gmail.com>
2026-03-31 00:41:29 +0100

    feat: added position monitoring nodes on settings page

--- backend/infrastructure/persistence/models/settings_repository.go
+++ backend/infrastructure/persistence/models/settings_repository.go
@@ -4,4 +4,5 @@ type SettingsRow struct {
 	DiscordWebhook string `db:"discord_webhook"`
 	SendOnFail     int64  `db:"send_on_fail"`
 	SendOnSuccess  int64  `db:"send_on_success"`
+	PositionNodes  string `db:"position_nodes"`
 }
--- backend/internal/core/settings/settings.go
+++ backend/internal/core/settings/settings.go
@@ -1,9 +1,15 @@
 package settings
 
 type Settings struct {
-	DiscordWebhook string `json:"discord_webhook"`
-	SendOnFail     bool   `json:"send_on_fail"`
-	SendOnSuccess  bool   `json:"send_on_success"`
+	DiscordWebhook string        `json:"discord_webhook"`
+	SendOnFail     bool          `json:"send_on_fail"`
+	SendOnSuccess  bool          `json:"send_on_success"`
+	PositionNodes  PositionNodes `json:"position_nodes"`
+}
+
+type PositionNodes struct {
+	HTTPNode string `json:"http_node"`
+	WSNode   string `json:"ws_node"`
 }
 
 type TestWebhook struct {
