cc87f4126b9a81a1ca3712829378b5e5e3bacc68
mbdev0 <mustafababar7@gmail.com>
2026-05-08 01:00:30 +0100

    feat: added decoding of buyevents + pool data for use in instructions

---
+++ backend/internal/solana/programs/pumpfun/pumpfun_amm/instructions/pool/request.go
@@ -0,0 +1,17 @@
+package pool
+
+import (
+	"context"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+)
+
+func GetPoolDataBytes(ctx context.Context, poolAddress solana.PK, httpClient *rpc.Client) ([]byte, error) {
+	resp, err := httpClient.GetAccountInfoWithOpts(ctx, poolAddress, &rpc.GetAccountInfoOpts{Encoding: solana.EncodingBase64, Commitment: rpc.CommitmentFinalized})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Value.Data.GetBinary(), nil
+}
