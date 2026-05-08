f71093bd541280bf32961c35cd26a2aca6f6d42d
mbdev0 <mustafababar7@gmail.com>
2026-05-08 01:00:41 +0100

    feat: added pda for pumpfun amm buys

---
+++ backend/internal/solana/programs/pumpfun/pumpfun_amm/pda/pda_amm.go
@@ -0,0 +1,226 @@
+package pda
+
+import (
+	"encoding/binary"
+	"personal_bot/internal/core/constants"
+
+	"github.com/gagliardetto/solana-go"
+)
+
+var (
+	pumpfun_amm_program = solana.MustPublicKeyFromBase58(constants.PumpFunAMMProgram)
+	pumpfun_program     = solana.MustPublicKeyFromBase58(constants.PumpFunProgram)
+)
+
+func getPoolAuthority(baseMint string) (solana.PK, error) {
+	poolAuth := []byte("pool-authority")
+
+	baseMintAddress, err := solana.PublicKeyFromBase58(baseMint)
+	if err != nil {
+		return solana.PK{}, err
+	}
+
+	seeds := [][]byte{poolAuth, baseMintAddress.Bytes()}
+
+	address, _, err := solana.FindProgramAddress(seeds, pumpfun_program)
+	return address, err
+}
+
+func GetPoolPda(creator string, baseMint string, quoteMint string) (string, error) {
+	poolAuth, err := getPoolAuthority(baseMint)
+	if err != nil {
+		return "", err
+	}
+
+	baseMintPubkey, err := solana.PublicKeyFromBase58(baseMint)
+	if err != nil {
+		return "", err
+	}
+
+	quoteMintPubkey, err := solana.PublicKeyFromBase58(quoteMint)
+	if err != nil {
+		return "", err
+	}
+
+	indexBytes := make([]byte, 2)
+	binary.LittleEndian.PutUint16(indexBytes, 0)
+
+	seeds := [][]byte{
+		[]byte("pool"),
+		indexBytes,
+		poolAuth.Bytes(),
+		baseMintPubkey.Bytes(),
+		quoteMintPubkey.Bytes(),
+	}
+
+	pubkey, _, err := solana.FindProgramAddress(seeds, pumpfun_amm_program)
+	if err != nil {
+		return "", err
+	}
+
+	return pubkey.String(), nil
+}
+
+func GetPoolTokenAccount(poolAddress string, mintAddress string, tokenAccount string) (string, error) {
+	pool, err := solana.PublicKeyFromBase58(poolAddress)
+	if err != nil {
+		return "", err
+	}
+
+	tokenaccount, err := solana.PublicKeyFromBase58(tokenAccount)
+	if err != nil {
+		return "", err
+	}
+	mint, err := solana.PublicKeyFromBase58(mintAddress)
+	if err != nil {
+		return "", err
+	}
+
+	poolTokenAccount, _, err := solana.FindAssociatedTokenAddressWithProgram(pool, mint, tokenaccount)
+	if err != nil {
+		return "", err
+	}
+
+	return poolTokenAccount.String(), nil
+}
+
+func GetProtocalFeeRecipientTokenAccount(protocolFeeRecipient, tokenProgram, quoteMint string) (string, error) {
+	pfa, err := solana.PublicKeyFromBase58(protocolFeeRecipient)
+	if err != nil {
+		return "", err
+	}
+
+	tokenprogram, err := solana.PublicKeyFromBase58(tokenProgram)
+	if err != nil {
+		return "", err
+	}
+
+	quotemint, err := solana.PublicKeyFromBase58(quoteMint)
+	if err != nil {
+		return "", err
+	}
+
+	address, _, err := solana.FindAssociatedTokenAddressWithProgram(pfa, quotemint, tokenprogram)
+
+	if err != nil {
+		return "", err
+	}
+	return address.String(), nil
+}
+
+func GetCoinCreatorVaultAuthority(creator string) (string, error) {
+	vault := []byte("creator_vault")
+	creatorBytes, err := solana.PublicKeyFromBase58(creator)
+	if err != nil {
+		return "", err
+	}
+	seeds := [][]byte{vault, creatorBytes.Bytes()}
+
+	address, _, err := solana.FindProgramAddress(seeds, pumpfun_amm_program)
+	if err != nil {
+		return "", err
+	}
+	return address.String(), nil
+}
+
+func GetCoinCreatorVaultAta(coinCreatorVaultAuthority, tokenProgram, wsolMint string) (string, error) {
+	coinCreatorVaultAuthorityBytes, err := solana.PublicKeyFromBase58(coinCreatorVaultAuthority)
+	if err != nil {
+		return "", err
+	}
+
+	tokenProgramBytes, err := solana.PublicKeyFromBase58(tokenProgram)
+	if err != nil {
+		return "", err
+	}
+
+	wsolMintBytes, err := solana.PublicKeyFromBase58(wsolMint)
+	if err != nil {
+		return "", err
+	}
+
+	address, _, err := solana.FindAssociatedTokenAddressWithProgram(coinCreatorVaultAuthorityBytes, wsolMintBytes, tokenProgramBytes)
+	if err != nil {
+		return "", err
+	}
+
+	return address.String(), nil
+}
+
+func GetUserVolumeAccumulatorAddress(wallet string) (string, error) {
+	walletBytes, err := solana.PublicKeyFromBase58(wallet)
+	if err != nil {
+		return "", err
+	}
+
+	userVolumeAccumulator := []byte("user_volume_accumulator")
+
+	seeds := [][]byte{userVolumeAccumulator, walletBytes.Bytes()}
+	address, _, err := solana.FindProgramAddress(seeds, pumpfun_amm_program)
+	if err != nil {
+		return "", err
+	}
+
+	return address.String(), nil
+}
+
+func GetUserVolumeAccumulatorWsolTokenAccount(userVolumeAccumulator, quoteTokenProgram, quoteMint string) (string, error) {
+	userVolumeAccumulatorBytes, err := solana.PublicKeyFromBase58(userVolumeAccumulator)
+	if err != nil {
+		return "", err
+	}
+
+	quoteTokenProgramBytes, err := solana.PublicKeyFromBase58(quoteTokenProgram)
+	if err != nil {
+		return "", err
+	}
+
+	quoteMintBytes, err := solana.PublicKeyFromBase58(quoteMint)
+	if err != nil {
+		return "", err
+	}
+
+	address, _, err := solana.FindAssociatedTokenAddressWithProgram(userVolumeAccumulatorBytes, quoteMintBytes, quoteTokenProgramBytes)
+
+	if err != nil {
+		return "", err
+	}
+
+	return address.String(), nil
+}
+
+func GetPoolV2Account(baseMint string) (string, error) {
+	poolV2 := []byte("pool-v2")
+	baseMintBytes, err := solana.PublicKeyFromBase58(baseMint)
+	if err != nil {
+		return "", err
+	}
+	seeds := [][]byte{poolV2, baseMintBytes.Bytes()}
+
+	address, _, err := solana.FindProgramAddress(seeds, pumpfun_amm_program)
+	if err != nil {
+		return "", err
+	}
+
+	return address.String(), nil
+}
+
+func GetBuybackFeeRecipientTokenAccount(quoteMint, buybackFeeRecipient, quoteTokenProgram string) (string, error) {
+
+	quoteMintAddy, err := solana.PublicKeyFromBase58(quoteMint)
+	if err != nil {
+		return "", err
+	}
+	buybackFeeRecipientAddy, err := solana.PublicKeyFromBase58(buybackFeeRecipient)
+	if err != nil {
+		return "", err
+	}
+
+	quoteTokenProgramAddy, err := solana.PublicKeyFromBase58(quoteTokenProgram)
+	if err != nil {
+		return "", err
+	}
+
+	address, _, err := solana.FindAssociatedTokenAddressWithProgram(quoteMintAddy, buybackFeeRecipientAddy, quoteTokenProgramAddy)
+	return address.String(), err
+}
