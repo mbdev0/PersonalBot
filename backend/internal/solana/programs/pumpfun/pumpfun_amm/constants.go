4f8ce00d1f3b908f6e5834d321501d286631b087
mbdev0 <mustafababar7@gmail.com>
2026-05-08 01:00:30 +0100

    feat: added decoding of buyevents + pool data for use in instructions

---
+++ backend/internal/solana/programs/pumpfun/pumpfun_amm/constants.go
@@ -0,0 +1,13 @@
+package pumpfunamm
+
+const (
+	GlobalConfig            = "ADyA8hdefvWN2dbGGWFotbzWxrAvLW83WG6QCVXvJKqw"
+	ProtocolFeeRecipient    = "7hTckgnGnLQR6sdH7YkqFTAA7VwTfYFaZ6EhEsU3saCX"
+	EventAuthority          = "GS4CU59F31iL7aR2Q8zVS8DRrcRnXX1yjQ66TqNVQnaR"
+	GlobalVolumeAccumulator = "C2aFPdENg4A2HQsmrd5rTw5TaYBX5Ku887cWjbFKtZpw"
+	FeeConfig               = "5PHirr8joyTMp9JMm6nW7hNDVyEYdkzDqazxPD7RaTjx"
+)
+
+var (
+	BuyExactQuoteInDiscriminator = []byte{198, 46, 21, 82, 180, 217, 232, 112}
+)
