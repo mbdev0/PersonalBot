eff18ae1b1664055ac531ec4adcaf687fda1d50a
mbdev0 <mustafababar7@gmail.com>
2026-05-11 00:45:52 +0100

    feat: added sell impl to transaction factory for pump

--- backend/internal/solana/programs/pumpfun/transaction/transaction_factory.go
+++ backend/internal/solana/programs/pumpfun/transaction/transaction_factory.go
@@ -9,6 +9,8 @@ import (
 	bondingcurve "personal_bot/internal/solana/programs/pumpfun/bonding_curve"
 	"personal_bot/internal/solana/programs/pumpfun/pda"
 	ammBuy "personal_bot/internal/solana/programs/pumpfun/pumpfun_amm/transaction/buy"
+	ammSell "personal_bot/internal/solana/programs/pumpfun/pumpfun_amm/transaction/sell"
+
 	"personal_bot/internal/solana/programs/pumpfun/pumpfun_native/transaction/buy"
 	"personal_bot/internal/solana/programs/pumpfun/pumpfun_native/transaction/sell"
 
@@ -16,11 +18,12 @@ import (
 )
 
 func GetTransactionType(ctx context.Context, task tasks.Task, posService *position.Service) (transaction.Transaction, error) {
+
 	switch t := task.(type) {
 	case *tasks.BuyTask:
 		return getBuyImplementation(ctx, t, posService)
 	case *tasks.SellTask:
-		return &sell.Transaction{Task: t, PositionService: posService}, nil
+		return getSellImplementation(ctx, t, posService)
 	}
 	return nil, fmt.Errorf("no transaction found for task: %s", task.Type())
 }
@@ -39,4 +42,20 @@ func getBuyImplementation(ctx context.Context, task *tasks.BuyTask, posService *
 		return nil, err
 	}
 	return &buy.Transaction{BuyTask: task, PositionService: posService}, nil
+}
+
+func getSellImplementation(ctx context.Context, task *tasks.SellTask, posService *position.Service) (transaction transaction.Transaction, err error) {
+	bondingCurveAddress, err := pda.GetBondingCurveAddress(task.GetToken(), constants.PumpFunProgram)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err, hasCompleted := bondingcurve.GetBondingCurveDataFromAddress(ctx, bondingCurveAddress, task.HttpClient())
+	if err != nil {
+		if hasCompleted {
+			return &ammSell.Transaction{SellTask: task, PositionService: posService}, nil
+		}
+		return nil, err
+	}
+	return &sell.Transaction{Task: task, PositionService: posService}, nil
 }
