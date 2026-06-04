7e8e4210e4d5e7e5849113a08cbcd764a0288775
mbdev0 <mustafababar7@gmail.com>
2026-06-04 20:12:17 +0100

    feat: added spam tasks to api layer

--- backend/api/dto/trading_task.go
+++ backend/api/dto/trading_task.go
@@ -12,6 +12,7 @@ const (
 	AFK  TradingType = "AFK"
 	BUY  TradingType = "BUY"
 	SELL TradingType = "SELL"
+	SPAM TradingType = "SPAM"
 )
 
 type TradingTask struct {
@@ -22,13 +23,17 @@ type TradingTask struct {
 	Slippage     float64              `json:"slippage" validate:"required,gte=0,lte=1"`
 	RPCGroupId   int64                `json:"rpc_group_id" validate:"required"`
 
-	Filters        *Filters           `json:"filters,omitempty" validate:"required_if=Type AFK"`
-	SellStrategies *[]SellStrategyDTO `json:"sell_strategies,omitempty" validate:"required_if=Type AFK,required_if=Type BUY"`
-	BuyAmount      *float64           `json:"buy_amount,omitempty" validate:"required_if=Type AFK,required_if=Type BUY"`
-	BuyFee         *float64           `json:"buy_fee,omitempty" validate:"required_if=Type AFK,required_if=Type BUY"`
-	SellAmount     *float64           `json:"sell_amount,omitempty" validate:"required_if=Type Sell"`
-	SellFee        *float64           `json:"sell_fee,omitempty"`
-	TokenAddress   *string            `json:"token_address,omitempty" validate:"required_if=Type SELL,required_if=Type BUY"`
+	Filters          *Filters           `json:"filters,omitempty" validate:"required_if=Type AFK"`
+	SellStrategies   *[]SellStrategyDTO `json:"sell_strategies,omitempty" validate:"required_if=Type AFK,required_if=Type BUY"`
+	BuyAmount        *float64           `json:"buy_amount,omitempty" validate:"required_if=Type AFK,required_if=Type BUY"`
+	BuyFee           *float64           `json:"buy_fee,omitempty" validate:"required_if=Type AFK,required_if=Type BUY"`
+	SellAmount       *float64           `json:"sell_amount,omitempty" validate:"required_if=Type Sell"`
+	SellFee          *float64           `json:"sell_fee,omitempty"`
+	TokenAddress     *string            `json:"token_address,omitempty" validate:"required_if=Type SELL,required_if=Type BUY"`
+	Retries          *uint16            `json:"retries,omitempty" validate:"gte=0"`
+	RetriesDelayMS   *uint32            `json:"retry_delay_ms,omitempty" validate:"gte=0"`
+	NumberOfSubTasks *uint32            `json:"no_of_tasks,omitempty" validate:"gte=0 required_if=Type SPAM"`
+	StartTime        *uint64            `json:"start_time,omitempty" validate:"gte=0 require_if=Type SPAM"`
 }
 
 func (t *TradingTask) Validate() error {
@@ -49,17 +54,21 @@ type TradingTaskResponse struct {
 	RPCGroup      string      `json:"rpc_group"`
 	RPCGroupID    int64       `json:"rpc_group_id"`
 
-	SellStrategies *[]SellStrategyDTO `json:"sell_strategies,omitempty"`
-	BuyAmount      *float64           `json:"buy_amount,omitempty"`
-	Filters        *Filters           `json:"filters,omitempty"`
-	SellFee        *float64           `json:"sell_fee,omitempty"`
-	BuyFee         *float64           `json:"buy_fee,omitempty"`
-	SellAmount     *float64           `json:"sell_amount,omitempty"`
-	SellTaskId     *int64             `json:"sell_task_id,omitempty"`
-	BuyTaskId      *int64             `json:"buy_task_id,omitempty"`
-	PositionId     *int64             `json:"position_id,omitempty"`
-	TokenAddress   *string            `json:"token_address,omitempty"`
-	TimeCreated    int64              `json:"time_created"`
+	SellStrategies   *[]SellStrategyDTO `json:"sell_strategies,omitempty"`
+	BuyAmount        *float64           `json:"buy_amount,omitempty"`
+	Filters          *Filters           `json:"filters,omitempty"`
+	SellFee          *float64           `json:"sell_fee,omitempty"`
+	BuyFee           *float64           `json:"buy_fee,omitempty"`
+	SellAmount       *float64           `json:"sell_amount,omitempty"`
+	SellTaskId       *int64             `json:"sell_task_id,omitempty"`
+	BuyTaskId        *int64             `json:"buy_task_id,omitempty"`
+	PositionId       *int64             `json:"position_id,omitempty"`
+	TokenAddress     *string            `json:"token_address,omitempty"`
+	TimeCreated      int64              `json:"time_created"`
+	Retries          *uint16            `json:"retries,omitempty"`
+	RetriesDelayMS   *uint32            `json:"retry_delay_ms,omitempty"`
+	NumberOfSubTasks *uint32            `json:"no_of_tasks,omitempty"`
+	StartTime        *uint64            `json:"start_time,omitempty"`
 }
 
 type TradingTaskPatch struct {
@@ -70,13 +79,17 @@ type TradingTaskPatch struct {
 	Slippage     *float64              `json:"slippage" validate:"omitempty,gte=0,lte=1"`
 	RPCGroupId   *int64                `json:"rpc_group_id" validate:"omitempty"`
 
-	Filters        *Filters           `json:"filters,omitempty"`
-	SellStrategies *[]SellStrategyDTO `json:"sell_strategies,omitempty"`
-	BuyAmount      *float64           `json:"buy_amount,omitempty" validate:"omitempty,gt=0"`
-	BuyFee         *float64           `json:"buy_fee,omitempty" validate:"omitempty,gte=0"`
-	SellAmount     *float64           `json:"sell_amount,omitempty" validate:"omitempty,gt=0"`
-	SellFee        *float64           `json:"sell_fee,omitempty" validate:"omitempty,gte=0"`
-	TokenAddress   *string            `json:"token_address,omitempty"`
+	Filters          *Filters           `json:"filters,omitempty"`
+	SellStrategies   *[]SellStrategyDTO `json:"sell_strategies,omitempty"`
+	BuyAmount        *float64           `json:"buy_amount,omitempty" validate:"omitempty,gt=0"`
+	BuyFee           *float64           `json:"buy_fee,omitempty" validate:"omitempty,gte=0"`
+	SellAmount       *float64           `json:"sell_amount,omitempty" validate:"omitempty,gt=0"`
+	SellFee          *float64           `json:"sell_fee,omitempty" validate:"omitempty,gte=0"`
+	TokenAddress     *string            `json:"token_address,omitempty"`
+	Retries          *uint16            `json:"retries,omitempty"`
+	RetriesDelayMS   *uint32            `json:"retry_delay_ms,omitempty"`
+	NumberOfSubTasks *uint32            `json:"no_of_tasks"`
+	StartTime        *uint64            `json:"start_time"`
 }
 
 func (t *TradingTaskPatch) Validate() error {
