8099d9ef7ec93a566798a1873a3080ea67503e26
mbdev0 <mustafababar7@gmail.com>
2026-06-08 22:31:29 +0100

    feat: option to have notimer work/timer work -> no timer will not precreate tasks while timer will

--- backend/internal/services/trading/strategy/spam.go
+++ backend/internal/services/trading/strategy/spam.go
@@ -5,6 +5,8 @@ import (
 	"personal_bot/internal/core/strategies"
 	"personal_bot/internal/core/tasks"
 	"personal_bot/pkg/logger"
+	"sync"
+	"time"
 )
 
 type Spam struct {
@@ -16,17 +18,62 @@ func NewSpamEngine(strategy Strategy) *Spam {
 }
 
 func (s *Spam) Run(ctx context.Context, task *strategies.Spam) {
-	for {
-		select {
-		case <-ctx.Done():
-			s.cancelSubTasks(task)
-			return
-		default:
-			for i := 0; i < int(task.NumberOfSubTasks); i++ {
-				go s.work(task)
-			}
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		if task.StartTime == 0 {
+			s.noTimerWork(ctx, task)
+		} else {
+			s.timerWork(ctx, task)
 		}
+		return
 	}
+
+}
+
+func (s *Spam) timerWork(ctx context.Context, task *strategies.Spam) {
+	precreatedTasks := []tasks.Task{}
+
+	for i := 0; i < int(task.NumberOfSubTasks); i++ {
+		bt, err := s.createBuyTask(task)
+		if err != nil {
+			logger.Error(err)
+			continue
+		}
+		precreatedTasks = append(precreatedTasks, bt)
+	}
+
+	select {
+	case <-time.After(time.Until(time.Unix(int64(task.StartTime), 0))):
+	case <-ctx.Done():
+		s.cancelSubTasks(task)
+		return
+	}
+
+	for _, t := range precreatedTasks {
+		go s.taskService.StartTask(t.Id())
+	}
+
+	go func() {
+		<-ctx.Done()
+		s.cancelSubTasks(task)
+	}()
+}
+
+func (s *Spam) noTimerWork(ctx context.Context, task *strategies.Spam) {
+	var wg sync.WaitGroup
+	for i := 0; i < int(task.NumberOfSubTasks); i++ {
+		wg.Go(func() {
+			s.work(task)
+		})
+	}
+
+	go func() {
+		wg.Wait()
+		<-ctx.Done()
+		s.cancelSubTasks(task)
+	}()
 }
 
 func (s *Spam) work(task *strategies.Spam) {
@@ -42,7 +89,9 @@ func (s *Spam) cancelSubTasks(task *strategies.Spam) {
 	tasks := s.taskService.GetTasksWithStrategyId(task.StrategyTaskId())
 	for _, t := range tasks {
 		err := s.taskService.StopTask(t.Id())
-		logger.Error(err)
+		if err != nil {
+			logger.Error(err)
+		}
 	}
 }
 
@@ -52,23 +101,37 @@ func (s *Spam) createBuyTask(task *strategies.Spam) (*tasks.BuyTask, error) {
 		return nil, err
 	}
 
+	opts := []tasks.Option{
+		tasks.WithComputeUnits(uint32(task.ComputeUnits)),
+		tasks.WithProgram(task.Program),
+		tasks.WithRPCGroupId(task.RPCGroupId()),
+		tasks.WithHttpNode(node.Http),
+		tasks.WithWS(node.WS),
+		tasks.WithSlippage(task.Slippage),
+		tasks.WithStrategyId(task.StrategyTaskId())}
+
+	if task.Retries != nil {
+		opts = append(opts, tasks.WithRetries(*task.Retries))
+	}
+
+	if task.RetriesDelayMS != nil {
+		opts = append(opts, tasks.WithRetriesDelayMs(*task.RetriesDelayMS))
+	}
+
 	bt := tasks.NewBuyTask(
 		task.Wallet,
 		task.Token,
-		[]tasks.Option{
-			tasks.WithComputeUnits(uint32(task.ComputeUnits)),
-			tasks.WithProgram(task.Program),
-			tasks.WithRPCGroupId(task.RPCGroupId()),
-			tasks.WithHttpNode(node.Http),
-			tasks.WithWS(node.WS),
-			tasks.WithSlippage(task.Slippage),
-			tasks.WithStrategyId(task.StrategyTaskId()),
-		},
+		opts,
 		[]tasks.BuyOption{
 			tasks.WithBuyAmount(task.BuyAmount),
 			tasks.WithBuyFee(task.BuyFee),
 		},
 	)
+
+	_, err = s.taskService.Create(bt)
+	if err != nil {
+		return nil, err
+	}
 
 	return bt, nil
 }
